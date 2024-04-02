39dd18a8e03a35d6ec0fc76525785727deae6cdd
aaronzaiman <aszaiman1@gmail.com>
2024-04-01 21:42:39 -0400

    Adding Log in page and sytling- will need to hook up to a backend

---
+++ src/Login.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+const Login: React.FC = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (event: React.FormEvent) => {
+        event.preventDefault();
+        // LOGIN LOGIC!!
+        alert(`Login attempt with username: ${username} and password: ${password}`);
+    };
+
+    return (
+        <div className="login-container">
+            <form className="login-form" onSubmit={handleSubmit}>
+                <label htmlFor="username">Username:</label>
+                <input 
+                id="username" 
+                type="text" 
+                value ={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required />
+                <label htmlFor="password">Password:</label>
+                <input 
+                id="password" 
+                type="password" 
+                value ={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
